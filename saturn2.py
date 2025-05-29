40eb92ae30ae99bf582d56b7b317f21a1d2cad1f
Bhaskar Jha <bhaskarjha95@gmail.com>
2025-05-29 15:55:11 +0530

    Update saturn2.py

---
+++ saturn2.py
@@ -0,0 +1,4 @@
+i am writing java code as i am learning from W3 schools.
+i guess they are having best way to teach topics in better way 
+they give platform to practice at the same time so that anyone can practice at the same time
+i will write code in saturn3.py for sure
