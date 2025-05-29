df73ab67cf5e5cd458b7f2b72277a5616bcd9610
Bhaskar Jha <bhaskarjha95@gmail.com>
2025-05-29 16:02:08 +0530

    Update saturn3.py

--- saturn3.py
+++ saturn3.py
@@ -1,8 +1,8 @@
-let write the code of java in python file 
+//let write the code of java in python file 
 
 public class Main {
     public static void main(String[] args) {
 System.out.println("Hello World");
 // this code each and every platform provides not because they can't write just they want to help in easy way
         }
-        }
+    }
